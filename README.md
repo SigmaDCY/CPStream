1f4f6f35c0946e419201e8082738478af5b54f74
SigmaDCY <dingdang77_2024@outlook.com>
2026-08-29 19:14:39 +0800

    添加生成文件的功能，添加codeforces和atcoder的支持，并更改交互模式，并将项目名称改为CPStream

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# CPCodeManager
-一个用于标准化归档信息竞赛代码的工具
+# CPStream
+From fetch to review, flow like a stream.
+从抓取到复习，一气呵成。
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+﻿#include <iostream>
 #include <filesystem>
 #include <fstream>
-#include "CLI11.hpp"
+#include <regex>
+#include <cstdlib>
 #include "httplib.h"
 #include "json.hpp"
 namespace fs = std::filesystem;
@@ -10,7 +11,7 @@ using json = nlohmann::json;
 struct Problem
 {
 	std::string name, group, url, id, inputType, outputType, subtaskSize;
-	int memoryLimit, timeLimit;
+	int memoryLimit = 0, timeLimit = 0;
 	struct TestCase
 	{
 		std::string input, output;
@@ -19,14 +20,38 @@ struct Problem
 
 	void extractIdFromUrl()
 	{
-		if (group == "Luogu") {
+		if (group == "Luogu")
+		{
+			// /P1001
 			size_t lastSlash = url.find_last_of('/');
-			if (lastSlash != std::string::npos) {
+			if (lastSlash != std::string::npos)
+			{
 				id = url.substr(lastSlash + 1);
 			}
 		}
+		else if (group == "Codeforces")
+		{
+			// /A/1
+			std::regex re("/problem/(\\d+)/([A-Za-z0-9]+)$");
+			std::smatch match;
+			if (std::regex_search(url, match, re))
+			{
+				id = match[1].str() + match[2].str();
+			}
+		}
+		else if (group == "AtCoder")
+		{
+			// /abc123_a
+			std::regex re("/tasks/([a-z0-9_]+)$");
+			std::smatch match;
+			if (std::regex_search(url, match, re))
+			{
+				id = match[1].str(); // 得到 "abc123_a"
+			}
+		}
 		// 在算出 id 之后，把所有不适合做文件夹名字的字符替换成下划线 '_'
-		for (char& c : id) {
+		for (char& c : id)
+		{
 			if (c == '/' || c == '\\' || c == ':' || c == '*' || c == '?' || c == '"' || c == '<' || c == '>' || c == '|') {
 				c = '_';
 			}
@@ -51,68 +76,102 @@ struct Problem
 			cppFile << "*/\n";
 			cppFile.close();
 		}
+		for (size_t i = 0; i < tests.size(); i++) {
+			fs::path inPath = dirPath / (std::to_string(i + 1) + ".in");
+			fs::path outPath = dirPath / (std::to_string(i + 1) + ".out");
+
+			std::ofstream inFile(inPath);
+			if (inFile.is_open())
+			{
+				inFile << tests[i].input;
+				inFile.close();
+			}
+
+			std::ofstream outFile(outPath);
+			if (outFile.is_open())
+			{
+				outFile << tests[i].output;
+				outFile.close();
+			}
+		}
 	}
 };
 
 int main(int argc, char* argv[])
 {
-	CLI::App app{ "CPCodeManager" };
-	auto fetchCmd = app.add_subcommand("fetch", "fetch problems' info");
-
-	int port = 12345;
-	fetchCmd->add_option("-p", port, "listening port"); //监听端口
-
-	try
+	const char* logo = R"( ██████╗██████╗ ███████╗████████╗██████╗ ███████╗ █████╗ ███╗   ███╗
+██╔════╝██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔════╝██╔══██╗████╗ ████║
+██║     ██████╔╝███████╗   ██║   ██████╔╝█████╗  ███████║██╔████╔██║
+██║     ██╔═══╝ ╚════██║   ██║   ██╔══██╗██╔══╝  ██╔══██║██║╚██╔╝██║
+╚██████╗██║     ███████║   ██║   ██║  ██║███████╗██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝     ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝
+                                                                    )";
+	std::cout << logo << std::endl;
+	while (true)
 	{
-		app.parse(argc, argv);
-	}
-	catch (const CLI::ParseError& e)
-	{
-		return app.exit(e);
-	}
-
-	if (app.get_subcommand(fetchCmd))
-	{
-		httplib::Server server; //声明监听服务器
-		server.Post("/", [](const httplib::Request& req, httplib::Response& res) //注册 POST 路由
-			{
-				res.set_content("OK", "text/plain");
-
-				try
-				{
-					Problem prob;
-					json data = json::parse(req.body);
-					prob.name = data.value("name", "unknown");
-					prob.group = data.value("group", "unknown");
-					prob.url = data.value("url", "unknown");
-					prob.memoryLimit = data.value("memoryLimit", -1);
-					prob.timeLimit = data.value("timeLimit", -1);
-					prob.extractIdFromUrl();
-					if (data.contains("input") && data["input"].is_object()) {
-						prob.inputType = data["input"].value("type", "unknown");
-					}
-					if (data.contains("output") && data["output"].is_object()) {
-						prob.outputType = data["output"].value("type", "unknown");
-					}
-					if (data.contains("tests") && data["tests"].is_array()) {
-						for (const auto& test : data["tests"]) {
-							Problem::TestCase tc;
-							tc.input = test.value("input", "");
-							tc.output = test.value("output", "");
-							prob.tests.push_back(tc);
-						}
-					}
-					prob.save();
-				}
-				catch (const json::parse_error& e)
-				{
-					std::cout << "Failed to parse json file!" << std::endl;
-				}
-			});
-		std::cout << "Ready to listening on 127.0.0.1:" << port << std::endl;
-		if (!server.listen("127.0.0.1", port))
+		std::cout << "cps> ";
+		std::string inst;
+		std::cin >> inst;
+		if (inst == "")
 		{
-			std::cout << "Failed to set handler!" << std::endl;
+			break;
+		}
+		if (inst[0] == '\0')
+		{
+			continue;
+		}
+		if (std::string(inst) == "fetch")
+		{
+			int port;
+			std::cin >> port;
+			if (port < 0 || port>65535)
+			{
+				std::cout << "请输入合法端口号！";
+			}
+			httplib::Server server;// 声明监听服务器
+			server.Post("/", [](const httplib::Request& req, httplib::Response& res) // 注册 POST 路由
+				{
+					res.set_content("OK", "text/plain");
+					try
+					{
+						Problem prob;
+						json data = json::parse(req.body);
+						prob.name = data.value("name", "unknown");
+						prob.group = data.value("group", "unknown");
+						prob.url = data.value("url", "unknown");
+						prob.memoryLimit = data.value("memoryLimit", -1);
+						prob.timeLimit = data.value("timeLimit", -1);
+						prob.extractIdFromUrl();
+						if (data.contains("input") && data["input"].is_object()) {
+							prob.inputType = data["input"].value("type", "unknown");
+						}
+						if (data.contains("output") && data["output"].is_object()) {
+							prob.outputType = data["output"].value("type", "unknown");
+						}
+						if (data.contains("tests") && data["tests"].is_array()) {
+							for (const auto& test : data["tests"]) {
+								Problem::TestCase tc;
+								tc.input = test.value("input", "");
+								tc.output = test.value("output", "");
+								prob.tests.push_back(tc);
+							}
+						}
+						prob.save();
+					}
+					catch (const json::parse_error& e)
+					{
+						std::cout << "Failed to parse json file!" << std::endl;
+					}
+				});
+			std::cout << "Ready to listening on 127.0.0.1:" << port << std::endl;
+			if (!server.listen("127.0.0.1", port))
+			{
+				std::cout << "Failed to set handler!" << std::endl;
+			}
+		}
+		else
+		{
+			std::cout << "未知命令" << std::endl;
 		}
 	}
 	return 0;
